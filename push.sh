2523a10294e41e19890c656ac5b7b14f12589181
chefdorv <chefdor.v@gmail.com>
2020-11-27 15:14:42 +0100

    after_success

--- push.sh
+++ push.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 git remote add cleverapps
-https://$CLEVER_TOKEN:$CLEVER_SECRET@push.clever-cloud.com/app_YOUR_APPLICATION_ID.git
+https://$CLEVER_TOKEN:$CLEVER_SECRET@push.clever-cloud.com/app_4120d72c-e5b5-4dd2-a45f-81da3482c8db.git
 git --verbose --force push cleverapps master
